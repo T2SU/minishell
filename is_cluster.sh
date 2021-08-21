1db22c73457de42664b555b3b965a05a77a96779
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-08-21 21:40:09 +0900

    필요 없는 파일 삭제

--- is_cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-if [ $(hostname | grep -Eo "c\d{1,2}r\d{1,2}s\d{1,2}.42seoul.kr") ]
-then
-	# echo "You are in 42 Seoul Cluster!"
-	return 1
-	else
-	# echo "You are not in 42 Seoul Cluster."
-	return 0
-fi
