4276d4881234f43e306ce098745b740d921003af
Haeun Kim <ckhe1215@gmail.com>
2021-09-12 17:56:44 +0900

    코드 약간 정리

--- srcs/command/cd_pwd.c
+++ srcs/command/cd_pwd.c
@@ -6,7 +6,7 @@
 /*   By: hkim <hkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 23:05:48 by hkim              #+#    #+#             */
-/*   Updated: 2021/09/12 17:28:18 by hkim             ###   ########.fr       */
+/*   Updated: 2021/09/12 17:56:18 by hkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,18 @@ int	command_run_cd(int argc, char *argv[], t_dict *dict)
 	char	path[PATH_MAX];
 
 	getcwd(path, PATH_MAX);
-	if (argc >= 2)
-	{
-		if (chdir(argv[1]) != -1)
-			return (change_pwd(dict, path));
-		raise_system_arg_error(argv[0], argv[1]);
-	}
-	else if (argc == 1)
+	if (argc == 1)
 	{
 		home = dict_get(dict, "HOME");
 		if (chdir(home) != -1)
 			return (change_pwd(dict, path));
 		raise_system_error(argv[0]);
+	}
+	else
+	{
+		if (chdir(argv[1]) != -1)
+			return (change_pwd(dict, path));
+		raise_system_arg_error(argv[0], argv[1]);
 	}
 	return (EXIT_FAILURE);
 }
